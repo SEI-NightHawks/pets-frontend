917f9ca6c4f9041e0379d19a3213ee925184e951
Demira11 <117664846+Demira11@users.noreply.github.com>
2023-12-06 16:34:23 -0500

    Added login and sign up's to nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,20 @@
-function Nav() {
-  return <p>This is the navbar</p>;
+import { NavLink } from "react-router-dom";
+
+function Nav({ title }) {
+  return (
+    <nav className="navbar">
+      <div className="nav-title">Petstagram</div>
+      {title === "home" ? (
+        <NavLink className="nav-item" to="/Login">
+          Login
+        </NavLink>
+      ) : (
+        <NavLink className="nav-item" to="/Signup">
+          Sign Up
+        </NavLink>
+      )}
+    </nav>
+  );
 }
 
 export default Nav;
