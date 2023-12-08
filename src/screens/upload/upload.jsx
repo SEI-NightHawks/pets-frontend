68b7ad83f881c1bfba4e8b2f0eb8c38ff016ee99
Demira11 <117664846+Demira11@users.noreply.github.com>
2023-12-08 16:12:26 -0500

    creating the upload page

--- src/screens/upload/upload.jsx
+++ src/screens/upload/upload.jsx
@@ -1,1 +1,18 @@
 import "./upload.css";
+import NavUpload from "../../components/Nav-upload.jsx";
+
+function Upload({ posts }) {
+  return (
+    <div>
+      <NavUpload />
+      <h1>This is the Feed page</h1>
+      <div>
+        {posts.map((post) => (
+          <Post post={post} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Upload;
