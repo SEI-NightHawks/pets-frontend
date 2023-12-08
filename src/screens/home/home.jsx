e4982eefb07dbd5502ee0a89b9e6d244393ece74
vricaurtec <vricaurte842@gmail.com>
2023-12-08 15:13:46 -0500

    deleted old home.jsx logic

--- src/screens/home/home.jsx
+++ src/screens/home/home.jsx
@@ -1,14 +1,9 @@
 import "./home.css";
 
-
 function Home() {
   return (
     <div>
       <p>This is the Home page</p>
-      <img src="" alt="" srcset="" onClick="<modal>
-      signPage 
-      
-      </modal>/>
     </div>
   );
 }
