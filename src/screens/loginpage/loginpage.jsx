294d6f9e9efcafa694cbce2fe8397da41c4c44c5
vricaurtec <vricaurte842@gmail.com>
2023-12-08 13:53:17 -0500

    sign up page jsx and basic css

--- src/screens/loginpage/loginpage.jsx
+++ src/screens/loginpage/loginpage.jsx
@@ -1,1 +1,51 @@
 import "./loginpage.css";
+
+// LoginPage.jsx
+
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'; // Import Link for React Router
+
+const LoginPage = ({ closeModal }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Handle login logic here
+    console.log('Logging in with:', username, password);
+    closeModal(); // Close the modal after login
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="button" onClick={handleLogin}>
+          Login
+        </button>
+        <br />
+        {/* Link to the SignUpPage component */}
+        <Link to="/signup">Sign Up</Link>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
--- src/screens/signup/signup.jsx
+++ src/screens/signup/signup.jsx
@@ -1,1 +1,102 @@
-import "./signup.css";
+
+// Signup.jsx
+
+import React, { useState } from 'react';
+import './signup.css'; // Import the CSS file
+
+const Signup = () => {
+  const [petUsername, setPetUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [petSpecies, setPetSpecies] = useState('');
+  const [breed, setBreed] = useState('');
+  const [dateOfBirth, setDateOfBirth] = useState('');
+  const [profilePicture, setProfilePicture] = useState(null);
+
+  const handleSignup = () => {
+    // Handle signup logic here
+    console.log('Signing up with:', petUsername, email, password, petSpecies, breed, dateOfBirth, profilePicture);
+  };
+
+  return (
+    <div className="signup-container">
+      <h2>Sign Up</h2>
+      <form>
+        <label>
+          Pet Username:
+          <input
+            type="text"
+            value={petUsername}
+            onChange={(e) => setPetUsername(e.target.value)}
+          />
+        </label>
+
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+
+        <label>
+          Pet Species:
+          <input
+            type="text"
+            value={petSpecies}
+            onChange={(e) => setPetSpecies(e.target.value)}
+          />
+        </label>
+
+        <label>
+          Breed:
+          <select
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+          >
+            <option value="">Select Breed</option>
+            {breedOptions.map((breedOption) => (
+         <option key={breedOption} value={breedOption}>
+             {breedOption}
+         </option>
+            ))}
+          </select>
+        </label>
+
+        <label>
+          Date of Birth:
+          <input
+            type="date"
+            value={dateOfBirth}
+            onChange={(e) => setDateOfBirth(e.target.value)}
+          />
+        </label>
+
+        <label>
+          Profile Picture:
+          <input
+            type="file"
+            accept="image/*"
+            onChange={(e) => setProfilePicture(e.target.files[0])}
+          />
+        </label>
+
+        <button type="button" onClick={handleSignup}>
+          Sign Up
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
