80e6f12182f9cd1938e30cbe9c62cbbf601a7ad1
Demira11 <117664846+Demira11@users.noreply.github.com>
2023-12-09 11:02:14 -0500

    installed bootstrap

--- src/screens/upload/upload.jsx
+++ src/screens/upload/upload.jsx
@@ -2,11 +2,45 @@ import "./upload.css";
 import NavUpload from "../../components/Nav-upload.jsx";
 
 function Upload() {
+  const [image, setImage] = useState();
+  const [progressBar, setProgressBar] = useState(0);
+
+  const handleFile = (event) => {
+    const file = event.target.files[0];
+
+    const formData = new formData();
+    setImage(URL.createObjectURL(file));
+    formdata.append("file", file);
+    axios
+      .post("url", formdata, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        onUploadProgress: (event) => {
+          setProgressBar(Math.round(100 * event.loaded) / event.total);
+        },
+      })
+      .then((res) => setImage(URL.createObjectURL(file)))
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="d-flex justify-content-center vh-100 bg-dark">
       <div className="bg-white p-5 rounded w-50 vh-80">
         <input type="file" onChange={handleFile} />
         <br /> <br />
+        <div className="progress">
+          <div
+            className="progress-bar progress-bar-stripped progress-bar-animated"
+            role="progressbar"
+            aria-label="progressbar"
+            aria-valuenow={progressBar}
+            aria-aria-valuemin="0"
+            aria-valuemax="100"
+            style={{ width: `${progressBar}%` }}
+          ></div>
+        </div>
+        <br />
+        {image && <img src={image} className="w-75 h-75"></img>}
       </div>
     </div>
   );
