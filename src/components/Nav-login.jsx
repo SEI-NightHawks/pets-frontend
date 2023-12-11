53a4c7ce8ae216af47402f31a10380cb6f59e45a
Demira11 <117664846+Demira11@users.noreply.github.com>
2023-12-11 14:32:55 -0500

    when on login page, click on image will take user home

--- src/components/Nav-login.jsx
+++ src/components/Nav-login.jsx
@@ -1,12 +1,36 @@
+// import React from "react";
+// import { NavLink } from "react-router-dom";
+// import "./Nav-login.css";
+
+// const NavLogin = () => {
+//   return (
+//     <nav className="navbar">
+//       <div className="nav-container">
+//         <div className="logo-image-login">
+//           <img src="images/pets.png" alt="pets-logo" className="logo-image" />
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default NavLogin;
+
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./Nav-login.css";
 
 const NavLogin = () => {
+  const navigate = useNavigate();
+
+  const handleImageClick = () => {
+    navigate("/");
+  };
+
   return (
     <nav className="navbar">
       <div className="nav-container">
-        <div className="logo-image-login">
+        <div className="logo-image-login" onClick={handleImageClick}>
           <img src="images/pets.png" alt="pets-logo" className="logo-image" />
         </div>
       </div>
