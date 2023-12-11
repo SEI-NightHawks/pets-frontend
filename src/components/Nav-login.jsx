5762c4c95131fd1211b63af324e0c277b1bd60a2
Demira11 <117664846+Demira11@users.noreply.github.com>
2023-12-11 14:36:34 -0500

    image on sign up page will take user home when clicked

--- src/components/Nav-login.jsx
+++ src/components/Nav-login.jsx
@@ -1,21 +1,3 @@
-// import React from "react";
-// import { NavLink } from "react-router-dom";
-// import "./Nav-login.css";
-
-// const NavLogin = () => {
-//   return (
-//     <nav className="navbar">
-//       <div className="nav-container">
-//         <div className="logo-image-login">
-//           <img src="images/pets.png" alt="pets-logo" className="logo-image" />
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default NavLogin;
-
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./Nav-login.css";
--- src/components/Nav-signup.jsx
+++ src/components/Nav-signup.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Nav-signup.css";
-import { NavLink } from "react-router-dom";
 
 const NavSignup = () => {
+  const navigate = useNavigate();
+
+  const handleImageClick = () => {
+    navigate("/");
+  };
+
   return (
     <nav className="navbar">
       <div className="nav-container">
-        <div className="logo-image-signup">
+        <div className="logo-image-signup" onClick={handleImageClick}>
           <img src="images/pets.png" alt="pets-logo" className="logo-image" />
         </div>
       </div>
