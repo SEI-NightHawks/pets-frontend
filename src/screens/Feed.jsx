fffdb057b7bc82c1994378a5635d0fc5cb7f7a7f
pondern <nicolasponder@gmail.com>
2023-12-13 11:15:46 -0500

    fix bug on delete user

--- src/screens/Feed.jsx
+++ src/screens/Feed.jsx
@@ -2,7 +2,7 @@ import Post from "../components/Post/Post.jsx";
 import NavRest from "../components/Nav-rest.jsx";
 import FloatingBottomNav from "../components/footer/FloatingBottomNav.jsx";
 
-function Feed({ posts }) {
+function Feed({ posts, user }) {
   const sortedPosts = posts.slice().sort((a, b) => {
     return new Date(b.created_at) - new Date(a.created_at);
   });
@@ -15,7 +15,7 @@ function Feed({ posts }) {
           <Post key={post.id} post={post} />
         ))}
       </div>
-      <FloatingBottomNav />
+      <FloatingBottomNav userId={user.user.id} />
     </div>
   );
 }
